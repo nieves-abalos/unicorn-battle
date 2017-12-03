789a889db118e56fd04b334e9b1038599076728b
Carlos Muñoz-Romero <carlosmunoz@gmail.com>
2017-12-03 01:57:02 +0100

    Update visualization and layout

--- front/js/unicorn-words.js
+++ front/js/unicorn-words.js
@@ -1,85 +1,256 @@
-var text_string = "Of course that’s your contention. You’re a first year grad student. You just got finished readin’ some Marxian historian, Pete Garrison probably. You’re gonna be convinced of that ’til next month when you get to James Lemon and then you’re gonna be talkin’ about how the economies of Virginia and Pennsylvania were entrepreneurial and capitalist way back in 1740. That’s gonna last until next year. You’re gonna be in here regurgitating Gordon Wood, talkin’ about, you know, the Pre-Revolutionary utopia and the capital-forming effects of military mobilization… ‘Wood drastically underestimates the impact of social distinctions predicated upon wealth, especially inherited wealth.’ You got that from Vickers, Work in Essex County, page 98, right? Yeah, I read that, too. Were you gonna plagiarize the whole thing for us? Do you have any thoughts of your own on this matter? Or do you, is that your thing? You come into a bar. You read some obscure passage and then pretend, you pawn it off as your own, as your own idea just to impress some girls and embarrass my friend? See, the sad thing about a guy like you is in 50 years, you’re gonna start doin’ some thinkin’ on your own and you’re gonna come up with the fact that there are two certainties in life. One: don’t do that. And two: you dropped a hundred and fifty grand on a fuckin’ education you coulda got for a dollar fifty in late charges at the public library.";
 
-
-function drawWordCloud( text_string ){
-
-var common = "poop,i,me,my,myself,we,us,our,ours,ourselves,you,your,yours,yourself,yourselves,he,him,his,himself,she,her,hers,herself,it,its,itself,they,them,their,theirs,themselves,what,which,who,whom,whose,this,that,these,those,am,is,are,was,were,be,been,being,have,has,had,having,do,does,did,doing,will,would,should,can,could,ought,i'm,you're,he's,she's,it's,we're,they're,i've,you've,we've,they've,i'd,you'd,he'd,she'd,we'd,they'd,i'll,you'll,he'll,she'll,we'll,they'll,isn't,aren't,wasn't,weren't,hasn't,haven't,hadn't,doesn't,don't,didn't,won't,wouldn't,shan't,shouldn't,can't,cannot,couldn't,mustn't,let's,that's,who's,what's,here's,there's,when's,where's,why's,how's,a,an,the,and,but,if,or,because,as,until,while,of,at,by,for,with,about,against,between,into,through,during,before,after,above,below,to,from,up,upon,down,in,out,on,off,over,under,again,further,then,once,here,there,when,where,why,how,all,any,both,each,few,more,most,other,some,such,no,nor,not,only,own,same,so,than,too,very,say,says,said,shall";
-
-var word_count = {};
-
-var words = text_string.split(/[ '\-\(\)\*":;\[\]|{},.!?]+/);
-    if (words.length == 1){
-    word_count[words[0]] = 1;
-    } else {
-    words.forEach(function(word){
-        var word = word.toLowerCase();
-        if (word != "" && common.indexOf(word)==-1 && word.length>1){
-        if (word_count[word]){
-            word_count[word]++;
-        } else {
-            word_count[word] = 1;
-        }
-        }
-    })
-    }
-
-var svg_location = "#chart";
-var width = $(document).width();
-var height = $(document).height()-400;
-
-// var color1 = '#b5ea77';
-// var color2 = '#4CAF50';
-var color1 = '#999999';
-var color2 = '#111111';
-var length = 200;
-
-var fill = d3.scale.category20();
-var color = d3.scale.linear().domain([1,length])
-                .interpolate(d3.interpolateHcl)
-                .range([d3.rgb(color2), d3.rgb(color1)]);
-
-var word_entries = d3.entries(word_count);
-
-var xScale = d3.scale.linear()
-    .domain([0, d3.max(word_entries, function(d) {
-        return d.value;
-    })
-    ])
-    .range([10,100]);
-
-d3.layout.cloud().size([width, height])
-    .timeInterval(20)
-    .words(word_entries)
-    .fontSize(function(d) { return xScale(+d.value); })
-    .text(function(d) { return d.key; })
-    // .rotate(function() { return ~~(Math.random() * 2) * 90; })
-    .rotate(function() { return 0; })
-    .font("Raleway")
-    .on("end", draw)
-    .start();
-
-function draw(words) {
-    d3.select(svg_location).append("svg")
-        .attr("width", width)
-        .attr("height", height)
-    .append("g")
-        .attr("transform", "translate(" + [width >> 1, height >> 1] + ")")
-    .selectAll("text")
-        .data(words)
-    .enter().append("text")
-        .style("font-size", function(d) { return xScale(d.value) + "px"; })
-        .style("font-family", "Raleway")
-        .style("fill", function(d, i) { return color(i); })
-        .attr("text-anchor", "middle")
-        .attr("transform", function(d) {
-        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-        })
-        .text(function(d) { return d.key; });
+var word_count = {
+    "gartner": 100,
+    "big data": 200,
+    "bootstrap": 200,
+    "cloud": 300,
+    "disruptive": 400,
+    "ecosystem": 100,
+    "freemium": 300,
+    "game changer": 200,
+    "gamification": 100,
+    "internet of things": 400,
+    "lean startup": 300,
+    "leverage": 100,
+    "lock-in effect": 100,
+    "long tail": 200,
+    "low hanging fruit": 100,
+    "mvp": 400,
+    "revenues estimates": 200,
+    "minimal viable product": 400,
+    "paradigm shift": 200,
+    "pivot": 200,
+    "tipping point": 100,
+    "traction": 300,
+    "viral marketing": 100,
+    "competition": 100,
+    "technology": 200,
+    "maping": 100,
+    "blockchain": 500,
+    "iot": 400,
+    "startup": 200,
+    "innovation": 200,
+    "api": 300,
+    "dataset": 200,
+    "machine learning": 500,
+    "voice recognition": 400,
+    "image recognition": 400,
+    "app": 200,
+    "application": 200,
+    "develop": 100,
+    "platform": 200,
+    "infrastructure": 200,
+    "automatic": 100,
+    "chatbot": 500,
+    "payment": 300,
+    "frontend": 200,
+    "backend": 200,
+    "mobile": 100,
+    "web": 100,
+    "connect": 100,
+    "software": 100,
+    "intelligent": 400,
+    "sensor": 300,
+    "analytics": 300,
+    "system": 100,
+    "digital": 100,
+    "assistant": 500,
+    "process": 100,
+    "monitor": 100,
+    "remote": 100,
+    "web services": 200,
+    "framework": 300,
+    "security": 100,
+    "computing": 100,
+    "design": 100,
+    "interoperability": 200,
+    "comunication": 100,
+    "gadget": 100,
+    "user": 100,
+    "algorithm": 300,
+    "architecture": 200,
+    "accessibility": 100,
+    "open source": 100,
+    "democratize": 400,
+    "direct democracy": 400,
+    "bank": 300,
+    "transport": 200,
+    "delivery": 300,
+    "parking": 200,
+    "mobility": 200,
+    "search": 100,
+    "bluetooth": 100
 }
 
-d3.layout.cloud().stop();
+function drawWordCloud( currentWords, callback ){
+
+    var mod_words = {}
+    for (var w in word_count){
+        if (word_count.hasOwnProperty(w)) {
+            if (word_count[w] > 400) mod_words[w] = parseFloat(word_count[w])*2;
+            mod_words[w] = parseFloat(word_count[w])/10;
+        }
+    }
+
+    var svg_location = "#chart";
+    var width = $(document).width();
+    var height = $(document).height()-400;
+
+    var color1 = '#999999';
+    var color2 = '#111111';
+    var length = 100;
+
+    var fill = d3.scale.category20();
+    var color = d3.scale.linear().domain([1,length])
+                    .interpolate(d3.interpolateHcl)
+                    .range([d3.rgb(color2), d3.rgb(color1)]);
+
+    var word_entries = d3.entries(word_count);
+
+    var xScale = d3.scale.linear()
+        .domain([0, d3.max(word_entries, function(d) {
+            return d.value;
+        })
+        ])
+        .range([5,100]);
+
+    d3.layout.cloud().size([width, height])
+        .timeInterval(20)
+        .words(word_entries)
+        .fontSize(function(d) { return xScale(+d.value); })
+        .text(function(d) { return d.key; })
+        // .rotate(function() { return ~~(Math.random() * 2) * 90; })
+        .rotate(function() { return 0; })
+        .font("Raleway")
+        .on("end", draw)
+        .start();
+
+    function draw(words) {
+        document.getElementById("chart").innerHTML = ""
+        d3.select(svg_location).append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .append("g")
+            .attr("transform", "translate(" + [width >> 1, height >> 1] + ")")
+            .selectAll("text")
+            .data(words)
+            .enter().append("text")
+            .style("font-size", function(d) { return xScale(d.value) + "px"; })
+            .style("font-family", "Raleway")
+            .style("fill", function(d, i) { 
+                if (currentWords) {
+                    if (d.key in currentWords) {
+                        return "#FFEB3B" 
+                    } else return color(i)
+                } else {
+                    console.info("no currentWords")
+                    return color(i); 
+                }
+            })
+            .style("stroke-width", function(d, i) { 
+                if (currentWords) {
+                    if (d.key in currentWords) return "1px"
+                }
+            })
+            .style("stroke", function(d, i) { 
+                if (currentWords) {
+                    if (d.key in currentWords) return "black"
+                }
+            })
+            .attr("text-anchor", "middle")
+            .attr("transform", function(d) {
+                return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+            })
+            .text(function(d) { return d.key; });
+            if (callback) callback()
+    }
+
+    d3.layout.cloud().stop();
+}
+
+var fua = new Audio('sound/fua.wav');
+
+var ranking = [
+    { id: 1, points: 300, score: 85 },
+    { id: 2, points: 200, score: 65 },
+    { id: 3, points: 120, score: 45 }
+]
+
+var current = {
+    id: 5,
+    points: 50,
+    score: 20,
+    words: {
+        "machine learning": 500,
+        "image recognition": 400,
+        "application": 200,
+        "platform": 200,
+        "automatic": 100,
+        "chatbot": 500
+    }
+}
+
+
+function update() {
+
+    setRanking(ranking, current);
+}
+
+function setRanking( ranking, current ) {
+    var indicators = document.getElementsByClassName("ranking-indicator");
+    var teams = document.getElementsByClassName("team");
+    var points = document.getElementsByClassName("points");
+    var unicorngif = document.getElementById("unicorn-gif");
+
+    if (current) {
+        indicators[0].setAttribute("style", "left: "+current.score+"%");
+        teams[0].innerHTML = "t"+current.id;
+        points[0].innerHTML = ""+current.points;
+        if (Object.keys(current.words).length > 0){
+
+            Object.assign(word_count, current.words);
+
+            drawWordCloud( current.words )
+
+            unicorngif.classList.toggle("hidden");
+            fua.play();
+            fua.currentTime = 0
+            setTimeout(function() {
+                unicorngif.classList.toggle("hidden");
+            }, 1200)
+        }
+    }
+
+    for (let i = 1; i < indicators.length; i++) {
+        indicators[i].setAttribute("style", "left: "+ranking[i-1].score+"%");
+        teams[i].innerHTML = ""+ranking[i-1].id;
+        points[i].innerHTML = ""+ranking[i-1].points;
+    }
+    
 }
 
 $( document ).ready(function() {
-    drawWordCloud(text_string);
+
+    drawWordCloud(null, update);
+
+    setTimeout( function() {
+        let newWords = {
+            id: 4,
+            points: 80,
+            score: 40,
+            words: {
+                "blockchain": 450
+            }
+        }
+        let prevWords = current.words;
+        Object.assign(prevWords, newWords.words);
+        Object.assign(current, newWords);
+        current.words = prevWords;
+        console.log(current)
+        
+        setRanking(ranking, current)
+
+    }, 3000)
+
+
 });
